5b6ba9d7ef707dc103e6b3ece46e287b302eb480
isaias59 <127485411+isaias59@users.noreply.github.com>
2024-04-10 22:10:37 -0700

    Update stack.hpp

--- src/stack.hpp
+++ src/stack.hpp
@@ -1,14 +1,64 @@
+#ifndef STACK_HPP
+#define STACK_HPP
+
+#include <string>
 #include <iostream>
-#include "stack.hpp" 
+using namespace std;
 
-int main() {
-    Stack stk; 
-    string line; 
+constexpr int STK_MAX = 1000; 
 
-    while (getline(std::cin, line)) { 
-        push_all(stk, line); 
-        pop_all(stk); 
+class Stack {
+    int _top; 
+    char buf[STK_MAX]; 
+
+public:
+    Stack() : _top(-1) {} 
+
+    void push(char c) {
+        if (!isFull()) {
+            buf[++_top] = c; 
+        } else {
+            cout << "Stack is full. Cannot push '" << c << "'\n";
+        }
     }
 
-    return 0; 
+    char pop() {
+        if (!isEmpty()) {
+            return buf[_top--]; 
+        } else {
+            cout << "Stack is empty. Cannot pop.\n";
+            return '@'; 
+        }
+    }
+
+    char top() {
+        if (!isEmpty()) {
+            return buf[_top]; 
+        } else {
+            cout << "Stack is empty. Top character not available.\n";
+            return '@'; 
+        }
+    }
+
+    bool isEmpty() {
+        return _top == -1; 
+    }
+
+    bool isFull() {
+        return _top == STK_MAX - 1; 
+    }
+};
+
+void push_all(Stack &stk, string line) {
+    for (char c : line) {
+        stk.push(c);
+    }
 }
+
+void pop_all(Stack &stk) {
+    while (!stk.isEmpty()) {
+        cout << stk.pop();
+    }
+    cout << '\n';
+}
+#endif 
