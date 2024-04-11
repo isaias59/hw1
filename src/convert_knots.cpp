a33c33fd3bf5b8aa906a073920ba5940ae39c98a
isaias59 <127485411+isaias59@users.noreply.github.com>
2024-04-10 17:09:43 -0700

    Update convert_knots.cpp

--- src/convert_knots.cpp
+++ src/convert_knots.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+#include "convert_knots.hpp"
+
+using namespace std;
+
+int main ()
+{
+    double knots;
+    cout << "\n ENTER YOUR KNOTTS: " << '\n';
+    cin >> knots;
+    cout << "\nHere is your knotts: " << knots_to_miles_per_minute(knots);
+
+    return 0;
+}
 
