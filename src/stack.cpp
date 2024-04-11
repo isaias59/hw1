d0bc96f569b9ad67ef91ec145cf0f70c4314847e
isaias59 <127485411+isaias59@users.noreply.github.com>
2024-04-10 17:02:14 -0700

    Update stack.cpp

--- src/stack.cpp
+++ src/stack.cpp
@@ -1,1 +1,41 @@
 
+#include <iostream>
+#include <string>
+#include <cctype> 
+#include "letter_count.hpp"
+
+constexpr int N_CHARS = 'Z' - 'A' + 1; 
+int char_to_index(char ch) {
+    return std::toupper(ch) - 'A';
+}
+
+char index_to_char(int i) {
+    return 'A' + i;
+}
+
+void count(std::string s, int counts[]) {
+    for (char ch : s) {
+        if (std::isalpha(ch)) { 
+            ++counts[char_to_index(ch)];
+        }
+    }
+}
+
+void print_counts(const int counts[], int len) {
+    for (int i = 0; i < len; ++i) {
+        std::cout << index_to_char(i) << ' ' << counts[i] << '\n';
+    }
+}
+
+int main() {
+    int counts[N_CHARS] = {0};
+    std::string line;
+
+    while (std::getline(std::cin, line)) {
+        count(line, counts);
+    }
+
+    print_counts(counts, N_CHARS);
+    return 0;
+}
+
