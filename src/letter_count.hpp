909c317b76d0356a5bf90277a6eccca628c0cdcc
isaias59 <127485411+isaias59@users.noreply.github.com>
2024-04-10 17:07:53 -0700

    Update letter_count.hpp

--- src/letter_count.hpp
+++ src/letter_count.hpp
@@ -1,1 +1,10 @@
+#ifndef LETTER_COUNT_HPP
+#define LETTER_COUNT_HPP
+#include <string>
 
+int char_to_index(char ch);
+char index_to_char(int i);
+void count(std::string s, int counts[]);
+void print_counts(const int counts[], int len);
+
+#endif
