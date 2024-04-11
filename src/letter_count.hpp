e2f03c3f1dbc1b275b3ec344e1a8bf2dcb360068
isaias59 <127485411+isaias59@users.noreply.github.com>
2024-04-10 23:13:59 -0700

    Update letter_count.hpp

--- src/letter_count.hpp
+++ src/letter_count.hpp
@@ -1,11 +1,35 @@
+
 #ifndef LETTER_COUNT_HPP
 #define LETTER_COUNT_HPP
 
 #include <string>
+#include <cctype>
+#include <iostream>
 
-int char_to_index(char ch);
-char index_to_char(int i);
-void count(std::string s, int counts[]);
-void print_counts(const int counts[], int len);
+constexpr int N_CHARS = ('Z'-'A'+1); 
+
+inline int char_to_index(char ch) {
+    return std::toupper(ch) - 'A';
+}
+
+inline char index_to_char(int i) {
+    return 'A' + i;
+}
+
+inline void count(std::string s, int counts[]) {
+    for (char ch : s) {
+        if (std::isalpha(ch)) {
+            int index = char_to_index(ch);
+            counts[index]++;
+        }
+    }
+}
+
+inline void print_counts(const int counts[], int len) {
+    for (int i = 0; i < len; ++i) {
+        std::cout << index_to_char(i) << ' ' << counts[i] << std::endl;
+    }
+}
 
 #endif
+
